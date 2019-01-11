f282535474c8fbbe074069db1c9532573fdc5317
Junhong Kim <junhong.kim@milliman.com>
2019-01-11 05:35:44 +0000

    add timer and show the timer

--- timer.js
+++ timer.js
@@ -1,3 +1,40 @@
 // Get current time
 let off_session = new Date();
 let work_session = new Date();
+
+// Set each session timer
+work_session.setMinutes(work_session.getMinutes() + 25);
+off_session.setMinutes(off_session.getMinutes() + 30);
+
+setInterval(function(){
+  let now = new Date().getTime(); 
+
+  // Find gap between target and now
+  let gap = work_session - now; 
+
+  // console.log(gap);
+
+  // Time calculation for minutes and seconds
+  let minutes = Math.floor((gap % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((gap % (1000 * 60)) / 1000);
+
+  // Transform minute and second in 2-digit
+  minutes = get2digitform(minutes);
+  seconds = get2digitform(seconds);
+
+  // console.log('seconds ', secocnds);
+
+  // Display the result in the html document
+  console.log(minutes, seconds); 
+  console.log(`${minutes} : ${seconds}`);
+
+  document.getElementById("session").innerText = `${minutes} : ${seconds}`;
+  
+}, 1000);
+
+function get2digitform(val){
+  return val < 10? '0' + val : val;
+}
+
+// to - do 
+// print digit in 2-digit form
